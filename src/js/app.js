1f978d68dc0552cee629c99c5b4f4012463f7c62
Juan Marco <jmmarco@gmail.com>
2019-05-22 10:22:41 -0300

    Highlight active navbar link

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 import './router'
+
+// Highlight active navbar element on click
+$(".nav-item").on("click", function(e) {
+  var $a = $(e.target)
+  var $ul = $a.parent().parent()
+  $ul.find('.active').removeClass('active')
+  $a.addClass('active')
+});
