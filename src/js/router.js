915deff9df3a16477c74710fc59ff2b809ea0ba0
Juan Marco <jmmarco@gmail.com>
2020-03-14 14:21:53 -0300

    Use window.location API

--- src/js/router.js
+++ src/js/router.js
@@ -1,28 +1,27 @@
-import crossroads from 'crossroads'
-import homeController from './controllers/homeController'
-import peopleController from './controllers/peopleController'
-import savedController from './controllers/savedController'
-import contactController from './controllers/contactController'
-
-crossroads.addRoute('#/', function() {
-  $('.main').load('/partials/_home.html', homeController)
-})
-
-crossroads.addRoute('#/people', function() {
-  $('.main').load('/partials/_people.html', peopleController)
-})
-
-crossroads.addRoute('#/saved', function() {
-  $('.main').load('/partials/_saved.html', savedController)
-})
-
-crossroads.addRoute('#/contact', function() {
-  $('.main').load('/partials/_contact.html', contactController)
-})
+// import crossroads from 'crossroads'
+import homeController from "./controllers/homeController";
+import peopleController from "./controllers/peopleController";
+import savedController from "./controllers/savedController";
+import contactController from "./controllers/contactController";
 
 // En cada cambio del # va a verificar las rutas
-$(window).on('hashchange', function () {
-  crossroads.parse(window.location.hash)
-})
+$(window).on("hashchange", locationHashChanged);
 
-crossroads.parse(window.location.hash)
+function locationHashChanged() {
+  switch (location.hash) {
+    case "#/":
+      $(".main").load("/partials/_home.html", homeController);
+      break;
+    case "#/contact":
+      $(".main").load("/partials/_contact.html", contactController);
+      break;
+    case "#/saved":
+      $(".main").load("/partials/_saved.html", savedController);
+      break;
+    case "#/people":
+      $(".main").load("/partials/_people.html", peopleController);
+      break;
+    default:
+      break;
+  }
+}
