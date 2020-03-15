4594d16f970fcce92365e3dd8d02c11e9fc7ebdb
Juan Marco <jmmarco@gmail.com>
2020-03-15 12:43:44 -0300

    Add webpack mini-css-extract-plugin to avoid FOUC

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 
 module.exports = {
@@ -19,7 +20,18 @@ module.exports = {
       // CSS Rule
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === 'development',
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+        ],
       },
       // Font Rule
       {
@@ -41,6 +53,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Star Wars API',
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ]
 }
